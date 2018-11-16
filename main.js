a8bde9d914b774e93c9b1d12585302f51046b317
Kacey Okafor <kaceyokafor@gmail.com>
2018-11-16 11:55:34 +0100

    Check if input matches selected color

--- main.js
+++ main.js
@@ -12,7 +12,27 @@ colors.sort();
 // Select a color in the colors array with the randomly generated number
 const target = colors[target_index]; 
 
-//Prompt the user to input a color
+// Prompt the user to input a color
 let guess_input = prompt("I am thinking of one of these colors: \n \n \
 blue, cyan, gold, gray, green, magenta, orange, red, white, yellow \n \n\
- What color am I thinking of? "); 
+What color am I thinking of? "); 
+
+let tries = 1; 
+
+// Check if the input matches the selected color and give hints.
+if( colors.includes(guess_input) && guess_input == target) {
+    const background = document.getElementById("div"); 
+    background.style.backgroundColor = guess_input; 
+    prompt(`Congratulations! You have guessed the color! \n\ It took you ${tries} guesses to finish the game! \
+    You can see the colour in the background.`);
+
+} else if ( colors.includes(guess_input) && guess_input < target ) {
+    
+    alert("Sorry, your guess is not correct! \n\ \n\ \
+Hint: Your color is alphabetically lower than mine \n\ \n\ Please try again.");
+} else if ( colors.includes(guess_input) && guess_input > target){
+    alert("Sorry, your guess is not correct! \n\ \n\ \
+Hint: Your color is alphabetically higher than mine \n\ \n\ Please try again.");
+} else {
+    alert( "Sorry, I don't recognize your color. \n\ \n\ Please try again.")
+} 
